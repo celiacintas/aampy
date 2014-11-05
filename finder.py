23b8573ed4edb7a91d2c2c940048363a4dda3400
Celita Cintas <cintas.celia@gmail.com>
2014-11-05 13:19:13 -0300

    pylinteado

--- finder.py
+++ finder.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+""" 
+Module for (after) generalize any haarcascade classiffier
+"""
+
+import os
+import sys
+from os.path import basename, splitext
 import cv2
 from cv2 import cv
-import numpy as np
 import argparse
-import os
-from os.path import basename, splitext
 
 
 class Finder(object):
@@ -15,25 +19,27 @@ class Finder(object):
         """Load the images and the haarcascade files"""
         super(Finder, self).__init__()
         self.cascade = cv2.CascadeClassifier(path_cascade)
-    
 
-    def get_roi(self, image):
+    def get_roi(self, img):
         """Get the ROI of each image with the parameters finded in paper EARS"""
-        rects = self.cascade.detectMultiScale(image, 1.2, 3, 0 |cv.CV_HAAR_FIND_BIGGEST_OBJECT |cv.CV_HAAR_DO_ROUGH_SEARCH
-                                  |cv.CV_HAAR_SCALE_IMAGE, (0, 0))
-        
+        rects = self.cascade.detectMultiScale(img, 1.2, 3, 0
+                                            |cv.CV_HAAR_FIND_BIGGEST_OBJECT
+                                            |cv.CV_HAAR_DO_ROUGH_SEARCH
+                                            |cv.CV_HAAR_SCALE_IMAGE,
+                                            (0, 0))
+
         if len(rects) == 0:
             return []
         rects[:, 2:] += rects[:, :2]
-        
+
         return rects
 
-    def draw_rectangles(self, image, rects):
-        """Draw rectangle of ROI"""
-        for x1, y1, x2, y2 in rects:
-            cv2.rectangle(image, (x1, y1), (x2, y2), (127, 255, 0), 2)
+def draw_rectangles(img, rects):
+    """Draw rectangle of ROI"""
+    for x_1, y_1, x_2, y_2 in rects:
+        cv2.rectangle(img, (x_1, y_1), (x_2, y_2), (127, 255, 0), 2)
 
-        return image    
+    return img
 
 
 if __name__ == '__main__':
@@ -45,18 +51,20 @@ if __name__ == '__main__':
     group.add_argument("--folder", dest="folder", default=None,
                         help='Pass the path of the file to be process')
     args = parser.parse_args()
-    
+
     if args.file:
         filename = [args.file]
     elif args.folder:
-        filename = map(lambda f: os.path.join(args.folder, f), os.listdir(args.folder))
+        filename = map(lambda f: os.path.join(args.folder, f),
+                        os.listdir(args.folder))
     else:
         parser.print_help()
         sys.exit(1)
 
-    images = [cv2.imread(path, 0) for path in filename]
+    IMAGES = [cv2.imread(path, 0) for path in filename]
     my_finder = Finder("haarcascades/haarcascade_mcs_leftear.xml")
-    for i, image in enumerate(images):
+    for i, image in enumerate(IMAGES):
         rect = my_finder.get_roi(image)
-        my_finder.draw_rectangles(image, rect)
-        cv2.imwrite('/tmp/lero_{}.jpg'.format(splitext(basename(filename[i]))[0]), image)
+        draw_rectangles(image, rect)
+        cv2.imwrite('/tmp/out{}.jpg'.format(splitext(basename(filename[i]))[0]),
+                                            image)
