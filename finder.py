1255d2e91e9e1bc35ddcba2666a202e0ca28b29f
Celita Cintas <cintas.celia@gmail.com>
2014-11-17 13:38:41 -0300

    move pre proc to finder module

--- finder.py
+++ finder.py
@@ -11,7 +11,14 @@ from os.path import basename, splitext
 import cv2
 from cv2 import cv
 import argparse
+import numpy as np
 
+
+class NoROIException(Exception):
+    """Exception for no ROI finded"""
+
+    def __init__(self):
+        Exception.__init__(self, "ROI not found")
 
 
 class Finder(object):
@@ -28,13 +35,28 @@ class Finder(object):
                                             |cv.CV_HAAR_DO_ROUGH_SEARCH
                                             |cv.CV_HAAR_SCALE_IMAGE,
                                             (0, 0))
+        try:
+            if len(rects) == 0:
+                raise NoROIException()
+        except NoROIException, e:
+            raise NoROIException()
+        else:
+            rects[:, 2:] += rects[:, :2]
+            print rects
+            return rects[0]
 
-        if len(rects) == 0:
-            print 'upsss'
-            return []
-        rects[:, 2:] += rects[:, :2]
-
-        return rects
+    def preprocess_image(self, img, rect):
+        """"""
+        offset = 50
+        # x_1, y_1, x_2, y_2  rect
+        tmp = img[rect[1] - offset :rect[3] + offset, rect[0] - offset: rect[2] + offset]
+        norm = cv2.normalize(tmp, tmp, 0, 255, cv2.NORM_MINMAX , cv2.CV_8UC1)
+        print norm
+        grad_y = cv2.Sobel(norm, cv2.CV_32F, 0, 1, ksize=3)
+        grad_x = cv2.Sobel(norm, cv2.CV_32F, 1, 0, ksize=3)
+        mag = cv2.magnitude(grad_x, grad_y)
+        print np.max(mag), mag.argmax(axis=0)
+        return mag
 
 def draw_rectangles(img, rects):
     """Draw rectangle of ROI"""
@@ -70,9 +92,15 @@ if __name__ == '__main__':
     # this was nice but kill the mem
     my_finder = Finder("haarcascades/haarcascade_mcs_leftear.xml")
     for file in filenames:
+        print file
         image = cv2.imread(file, 0)
-        rect = my_finder.get_roi(image)
-        draw_rectangles(image, rect)
-        cv2.imwrite('/tmp/out{}.jpg'.format(splitext(basename(file))[0]),
-                                            image)
-
+        try:
+            rect = my_finder.get_roi(image)
+            mag = my_finder.preprocess_image(image, rect)
+            cv2.imwrite('/tmp/out{}.jpg'.format(splitext(basename(file))[0]),
+                                            mag)
+        except NoROIException, e:
+            print e
+            
+        #draw_rectangles(image, rect)
+        
